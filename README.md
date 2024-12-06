93bca73d5f186a5ba9636a20b5ae706ac339dc34
cibere <71997063+cibere@users.noreply.github.com>
2024-12-05 16:13:30 -0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Flow.Launcher.Plugin.FloginTemplate
+A basic template for creating a flow launcher plugin using [flogin](https://github.com/cibere/flogin)
---
+++ main.py
@@ -0,0 +1,11 @@
+import os
+import sys
+
+parent_folder_path = os.path.abspath(os.path.dirname(__file__))
+sys.path.append(parent_folder_path)
+sys.path.append(os.path.join(parent_folder_path, "lib"))
+
+from plugin.core import plugin
+
+if __name__ == "__main__":
+    plugin.run()
