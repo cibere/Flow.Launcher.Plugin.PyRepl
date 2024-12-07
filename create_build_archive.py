816ff6022a4483e4b7dcaf6174d4a0d081a2ce43
cibere <71997063+cibere@users.noreply.github.com>
2024-12-07 13:49:48 -0800

    update zip process

---
+++ create_build_archive.py
@@ -0,0 +1,14 @@
+import zipfile, click
+
+FILES = ("plugin.json", "main.exe", "icon.png")
+
+@click.command()
+@click.argument("name")
+def create_archive(name: str):
+    with zipfile.ZipFile(name, "w") as zf:
+        for file in FILES:
+            zf.write(file)
+            print(f"Added {file}")
+
+if __name__ == "__main__":
+    create_archive()
