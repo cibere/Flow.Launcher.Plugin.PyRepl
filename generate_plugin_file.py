6dc23404eb6be940b309c6adc5149a73a01b60f7
cibere <71997063+cibere@users.noreply.github.com>
2024-12-06 22:13:33 -0800

    Update generate_plugin_file.py

--- generate_plugin_file.py
+++ generate_plugin_file.py
@@ -3,15 +3,15 @@ import json
 with open("plugin.json", "r") as f:
     data = json.load(f)
 
-data['ID'] = "7ed41d7e3d174379972e1cdacb647474"
-data['Name'] = "Worknik Dictionary"
-data['Website'] = f"https://github.com/cibere/Flow.Launcher.Plugin.WordNikDictionary/tree/v{data['Version']}"
-data['ActionKeyword'] = "def"
-data['Language'] = "Executable_v2"
-data['ExecuteFileName'] = "pyrepl.exe"
+data['ID'] = "8d7d2301-4d69-4da2-a19b-e51c8ea239a9"
+data['Name'] = "PyRepl"
+data['Website'] = f"https://github.com/cibere/Flow.Launcher.Plugin.PyRepl/tree/v{data['Version']}"
+data['ActionKeyword'] = "py"
+data['Language'] = "executable_v2"
+data['ExecuteFileName'] = "main.exe"
 
 with open("plugin.json", "w") as f:
-    json.dump(data, f)
+    json.dump(data, f, indent=4)
 
 print("New plugin.json contents:")
 print(json.dumps(data, indent=4))
